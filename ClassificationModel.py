a382034fdbe3f44458e0d1c46d6f7e753062ad12
Skyler Maxwell <skylermaxwell42@gmail.com>
2019-03-22 22:16:43 -0400

    Adding Initial files

---
+++ ClassificationModel.py
@@ -0,0 +1,11 @@
+
+class ClassificationModel:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        return
+
+    def train(self):
+        return
+
+    def evaluate(self):
+        return
