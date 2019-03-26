680fac77ffeba0d1d487f58f38922861783c1578
Skyler Maxwell <maxwels2@tcnj.edu>
2019-03-25 23:58:46 -0400

    WIP

--- activate.sh
+++ activate.sh
@@ -1,5 +1,5 @@
 # Shell script to activate the python virtual environment and set path variables
-export PYTHONPATH=$PYTHONPATH:$(pwd)/scripts/TFUtils/models/:$(pwd)/scripts/TFUtils/models/research/:$(pwd)/scripts/TFUtils/models/research/slim/:$(pwd)/scripts/TFUtils/models/research/gan:
+export PYTHONPATH=$(pwd)/scripts/TFUtils/models/:$(pwd)/scripts/TFUtils/models/research/:$(pwd)/scripts/TFUtils/models/research/slim/:$(pwd)/scripts/TFUtils/models/research/gan:
 
 source tf-utils-venv/bin/activate
 
