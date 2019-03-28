cee188fb45a6cebe8c595e8810cd942c550119f9
Skyler Maxwell <skylermaxwell42@gmail.com>
2019-03-28 19:26:05 -0400

    Updating training script

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,13 +6,6 @@ import importlib
 import numpy as np
 from keras import backend as K
 import tensorflow as tf
-with K.tf.device('/gpu:1'):
-    config = tf.ConfigProto(intra_op_parallelism_threads=4,\
-        inter_op_parallelism_threads=4, allow_soft_placement=True,\
-        device_count = {'CPU' : 1, 'GPU' : 1})
-    session = tf.Session(config=config)
-    K.set_session(session)
-import tensorflow as tf
 
 import inspect
 import IMPL_Models
@@ -39,6 +32,11 @@ def parse_args():
                         choices=IMPL_Models.__all__,
                         type=str)
 
+    ogroup = parser.add_argument_group('Optional Argument')
+
+    ogroup.add_argument('--gpu',
+                        help='Flag: When set GPUs will be used for accelerated training')
+
     args = parser.parse_args()
 
     return args
@@ -46,7 +44,14 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-
+    if args.gpu:
+        with K.tf.device('/gpu:1'):
+            print('Using GPU')
+            config = tf.ConfigProto(intra_op_parallelism_threads=4,\
+                inter_op_parallelism_threads=4, allow_soft_placement=True,\
+                device_count = {'CPU' : 1, 'GPU' : 1})
+            session = tf.Session(config=config)
+            K.set_session(session)
 
     ds_info = load_data_set_path_dict()[args.dataset]
 
@@ -54,23 +59,6 @@ if __name__ == '__main__':
 
     print('----- Data Set: \n{}'.format(dataset))
 
-    # Loading images as np arrays
-    #train_data = np.asarray(dataset.train_images)
-    #train_labels = tf.one_hot(
-    #    indices=np.asarray(dataset.train_labels).astype(np.int32),
-    #     depth=dataset.num_classes)
-
-    #val_data = np.asarray(dataset.test_images)
-    #val_labels = tf.one_hot(
-    #    indices=np.asarray(dataset.test_labels).astype(np.int32),
-    #    depth=dataset.num_classes)
-
-    #print('----- Images Loaded: \nTrain: {}\nTest: {}'.format(
-        #len(train_data), len(val_data)
-    #))
-
     model = IMPL_Models.load_model(args.model)
-    #print(model)
-    #print(val_data.shape)
-    #print(val_labels)
+
     results = model.fit_gen(dataset.train_dir, dataset.validation_dir, 2)
